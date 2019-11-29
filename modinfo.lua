28629e66d487eae8e0d04dea3671af57040da144
Jango Course <jango.course@gmail.com>
2019-11-29 03:00:56 +0100

    First public release!

--- modinfo.lua
+++ modinfo.lua
@@ -1,19 +1,19 @@
 --Core PvP (C) 2019 Shrooblord
 meta =
 {
-    id = "CorePvP",
+    id = "1923214209", --1923214209 on WS
     name = "CorePvP",
     title = "Core PvP",
-    description = "[Server Admins] Designate a section of the Galaxy as PvP zone; in the rest of the Galaxy, PvP is disabled.",
+    description = "Designate a section of the Galaxy as PvP zone; in the rest of the Galaxy, PvP is disabled.",
     authors = {"Shrooblord"},
-    version = "0.1.0",
+    version = "1.0.0",
 
     dependencies = {
         {id = "Avorion", min = "0.29", max = "0.29.2"},
 
         --[[Shrooblord]]
-        --{id = "1847767864", min = "1.0.0"},              --ShrooblordMothership (library mod)
-        {id = "ShrooblordMothership", min = "1.1.0"},              --ShrooblordMothership (library mod)
+        {id = "1847767864", min = "1.1.0"},              --ShrooblordMothership (library mod)
+        --{id = "ShrooblordMothership", min = "1.1.0"},              --ShrooblordMothership (library mod)
     },
 
     serverSideOnly = false,
