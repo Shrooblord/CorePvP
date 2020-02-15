303ce9c63b4d55375bfc564a89a4410dd7311792
Jango Course <jango.course@gmail.com>
2020-02-15 10:41:20 +0100

    - Updated to Avo 0.32.

--- modinfo.lua
+++ modinfo.lua
@@ -7,10 +7,10 @@ meta =
     description = "Designate a section of the Galaxy as PvP zone; in the rest of the Galaxy, PvP is disabled.",
     authors = {"Shrooblord"},
 
-    version = "1.0.1",
+    version = "1.0.2",
     dependencies = {
         {id = "1847767864", min = "1.1"},  --Shrooblord Mothership
-        {id = "Avorion", min = "0.29", max = "0.31.*"}
+        {id = "Avorion", min = "0.29", max = "0.32.*"}
     },
 
     serverSideOnly = false,
