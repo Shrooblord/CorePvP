2c257e3282baadb8b03f2fb3ecf1eca603e12510
Jango Course <jango.course@gmail.com>
2020-03-30 15:19:39 +0200

    - Added command for Server Admins to force the state of a PvP/PvE zone, regardless of automatic settings. It also allows you to restore the automatic settings, in case you want to remove the forcing later. This also forces Neutral Zones into PvP mode, and restores them to previous behaviour if the force is cleared later.

--- data/scripts/commands/forcePvP.lua
+++ data/scripts/commands/forcePvP.lua
@@ -1,19 +1,43 @@
 package.path = package.path .. ";data/scripts/lib/?.lua"
 
 function execute(sender, commandName, state)
-    if not state
-        return 1, "", "You have to specify true or false!"
+    local player = Player()
+    if not player then
+        return 1, "", "Player invalid!"
+    end
+    
+    if not state then
+        return 1, "", "You have to specify true, false, or clear!"
     end
 
-    sector = Sector()
-    if not sector then
-        return 1, "", "Sector invalid!"
+    local cx, cy = player:getSectorCoordinates()
+
+    local code = [[
+        function forceEnablePvP(state)
+            local sector = Sector()
+            if not sector then
+                eprint("Sector invalid!")
+                return
+            end
+
+            if state == "clear" then
+                state = nil
+            end
+
+            sector:setValue("corePvP_forceEnabled_PvP", state)
+
+            sector:addScriptOnce("data/scripts/sector/corePvP.lua")
+            sector:invokeFunction("data/scripts/sector/corePvP.lua", "performDelayedInit")
+        end
+    ]]
+
+    local success = runSectorCode(cx, cy, true, code, "forceEnablePvP", state)
+
+    if success == 0 then
+        return 0, "", ""
+    else
+        return 1, "", "Sector not loaded into memory!"
     end
-
-    sector.setValue(corePvP_forceEnabled_PvP, state)
-    sector:addScriptOnce("data/scripts/sector/corePvP.lua")
-
-    return 0, "", ""
 end
 
 function getDescription()
@@ -21,5 +45,5 @@ function getDescription()
 end
 
 function getHelp()
-    return "Forces the PvP state of the Sector, overriding automatic procedures. Usage: /forcePvP <true|false>"
+    return "Forces the PvP state of the Sector, overriding automatic procedures. If run with 'clear' as the argument, clears any previous forcing and returns to automatic procedure. Usage: /forcePvP <true|false|clear>"
 end
